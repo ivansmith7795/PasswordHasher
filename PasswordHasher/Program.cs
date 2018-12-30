7d5bca7f214f0fb3134ec1983df852a9d0c2f3dc
ivansmith7795 <ivansmith@clarsentia.com>
2018-12-29 17:30:00 -0700

    Add project files.

---
+++ PasswordHasher/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+namespace PasswordHasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] password = Encoding.UTF8.GetBytes("gibson1973$");
+            byte[] salt = getSalt();
+
+            byte[] saltedHash = GenerateSaltedHash(password, salt);
+
+            Debug.WriteLine(Convert.ToBase64String(saltedHash));
+            Debug.WriteLine(Convert.ToBase64String(salt));
+            
+            Console.ReadLine();
+        }
+
+        private static byte[] getSalt()
+        {
+            var random = new RNGCryptoServiceProvider();
+
+            // Maximum length of salt
+            int max_length = 32;
+
+            // Empty salt array
+            byte[] salt = new byte[max_length];
+
+            // Build the random bytes
+            random.GetNonZeroBytes(salt);
+
+            // Return the string encoded salt
+            return salt;
+        }
+
+        static byte[] GenerateSaltedHash(byte[] plainText, byte[] salt)
+        {
+            HashAlgorithm algorithm = new SHA256Managed();
+
+            byte[] plainTextWithSaltBytes =
+              new byte[plainText.Length + salt.Length];
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                plainTextWithSaltBytes[i] = plainText[i];
+            }
+            for (int i = 0; i < salt.Length; i++)
+            {
+                plainTextWithSaltBytes[plainText.Length + i] = salt[i];
+            }
+
+            return algorithm.ComputeHash(plainTextWithSaltBytes);
+        }
+
+        public static bool CompareByteArrays(byte[] array1, byte[] array2)
+        {
+            if (array1.Length != array2.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
